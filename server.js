623fb7268d50a5dca57673329b213dbc52defb3f
frantz felix <frantz.felix@forgeglobal.com>
2022-06-07 13:24:47 -0700

    Added dotenv package and required it in the server.

--- server.js
+++ server.js
@@ -3,17 +3,17 @@
 const express = require(`express`);
 const morgan = require(`morgan`);
 const path = require(`path`);
-const routes = require(`./routes`)
+const routes = require(`./routes`);
+require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(morgan(`dev`));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(routes);
-  
 
-app.listen(PORT, function(){
-    console.log(`Server running at http://localhost:${PORT}`);
-})
+app.listen(PORT, function () {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
