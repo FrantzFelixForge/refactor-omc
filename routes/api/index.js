320bbdbaa2c1727e4bba032c8177509ab21613c6
frantz felix <frantz.felix@forgeglobal.com>
2022-06-07 15:53:58 -0700

    updated route to send back token in response

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
 
-router.use(`/`, function (req, res) {
-  res.status(200).json(`Api / endpoint`);
+router.get(`/success`, function (req, res) {
+  res
+    .status(200)
+    .json(`Api /success endpoint. The auth token is: ${req.params.code}`);
+});
+router.get(`/failure`, function (req, res) {
+  res.status(400).json(`Api /failure endpoint. No auth token.`);
 });
 
 module.exports = router;
