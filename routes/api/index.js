c175a9d5d999d1c8f137f6da20bfb8a22fe050d6
frantz felix <frantz.felix@forgeglobal.com>
2022-06-07 16:16:41 -0700

    Successfully read auth token and auth state from req query params when Asana redirects successfully.

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,7 +3,9 @@ const router = require("express").Router();
 router.get(`/success`, function (req, res) {
   res
     .status(200)
-    .json(`Api /success endpoint. The auth token is: ${req.params.code}`);
+    .json(
+      `Api /success endpoint. The auth token is: ${req.query.code} The state is: '${req.query.state}'`
+    );
 });
 router.get(`/failure`, function (req, res) {
   res.status(400).json(`Api /failure endpoint. No auth token.`);
