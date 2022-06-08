4db3423f3b931f76593166a0b44cb2a980983fcc
frantz felix <frantz.felix@forgeglobal.com>
2022-06-08 09:10:50 -0700

    Added a route to test getting the auth code from the client.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,25 @@
 const router = require("express").Router();
+require(`dotenv`).config();
 
-router.get(`/success`, function (req, res) {
-  res
-    .status(200)
-    .json(
-      `Api /success endpoint. The auth token is: ${req.query.code} The state is: '${req.query.state}'`
-    );
+router.get(`/authCode`, function (req, res) {
+  console.log(
+    `Api /authCode endpoint. The auth code is: ${req.query.code} The state is: '${req.query.state}'`
+  );
+  res.status(200).json();
+});
+
+router.post(`/bearerToken`, function (req, res) {
+  console.log(req.body);
+
+  const bearerRequestObj = {
+    grant_type: "authorization_code",
+    client_id: "1202402089879179",
+    client_secret: process.env.CLIENT_SECRET,
+    redirect_uri: "http://localhost:3000/",
+    code: req.body.code,
+    // code_verifier: "fdsuiafhjbkewbfnmdxzvbuicxlhkvnemwavx",
+  };
+  res.json(bearerRequestObj);
 });
 router.get(`/failure`, function (req, res) {
   res.status(400).json(`Api /failure endpoint. No auth token.`);
