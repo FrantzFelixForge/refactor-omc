29a87e2b47e04eb8299a8456aaf73d97e2a06244
frantz felix <frantz.felix@forgeglobal.com>
2022-06-08 18:15:04 -0700

    Added eventlistener to submit input, this ends up being the name of the task in asana

--- public/js/createAsanaTask.js
+++ public/js/createAsanaTask.js
@@ -1,9 +1,25 @@
 const taskNameInputEl = document.getElementById("taskNameInput");
 const taskFormEl = document.getElementById("taskForm");
-// const response = postDataSync(`url`, data);
 // console.log(response, "\n ^response^");
+async function postDataSync(url, data) {
+  console.log(data);
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  return await response.json();
+}
 
 taskFormEl.addEventListener("submit", function (e) {
   e.preventDefault();
-  console.log("Form submission");
+
+  const response = postDataSync(`/api/tasks/`, {
+    taskName: taskNameInputEl.value,
+  });
+  taskNameInputEl.value = "";
+  console.log("Form submission \n", response);
 });
