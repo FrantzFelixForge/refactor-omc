3961a7e239ded0712601ca635b274adfb2b6e2a9
frantz felix <frantz.felix@forgeglobal.com>
2022-06-22 14:13:58 -0700

    Added function to add a task in the correct project with some dummy data to contruct the name.

--- server.js
+++ server.js
@@ -128,7 +128,6 @@ async function findProject(teamGID) {
         choices: choiceArray,
       },
     ]);
-    console.log(choiceObj);
     return choiceObj[answers.project];
   } catch (error) {
     console.log(error);
@@ -136,6 +135,45 @@ async function findProject(teamGID) {
   }
 }
 
+async function addDeal(workspaceGID, projectGID, userGID, dealInfo) {
+  const newDeal = {
+    data: {
+      approval_status: "pending",
+      assignee: `${userGID}`,
+      assignee_status: "upcoming",
+      completed: false,
+      // due_on: `${req.body.taskCompletionDate}`,
+      //liked: true,
+      name: `${dealInfo.issuer} | ${dealInfo.seller}/${dealInfo.buyer} | $${dealInfo.price}`,
+      notes: `Term Sheet notes here`,
+      projects: [`${projectGID}`],
+      resource_subtype: "default_task",
+      //start_on: todaysDate,
+      parent: null,
+      workspace: `${workspaceGID}`,
+      //custom_fields: req.body.custom_fields,
+    },
+  };
+  try {
+    const response = await fetch(`https://app.asana.com/api/1.0/tasks`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.PERSONAL_ACCESS_TOKEN}`,
+      },
+      body: JSON.stringify(newDeal),
+    });
+    const data = await response.json();
+
+    console.log(data);
+    //res.status(201).json(data);
+    return data;
+  } catch (err) {
+    console.error(err);
+    res.status(400).json(err);
+  }
+}
+async function getDeal() {}
 async function mainMenu() {
   let answers = await inquirer.prompt([
     {
@@ -145,48 +183,8 @@ async function mainMenu() {
       choices: ["Get all deals?", "Add a deal?"],
     },
   ]);
-  return answers;
+  return answers.menu;
 }
-// async function addDeal(workspaceGID, userGID, projectGID, dealInfo) {
-//   const newTask = {
-//     data: {
-//       approval_status: "pending",
-//       assignee: `${userGID}`,
-//       assignee_status: "upcoming",
-//       completed: false,
-//       // due_on: `${req.body.taskCompletionDate}`,
-//       //liked: true,
-//       name: `${dealInfo.issuer} | ${dealInfo.seller}/${dealInfo.buyer} | $${dealInfo.price}`,
-//       notes: `Term Sheet notes here`,
-//       projects: [`${projectGID}`],
-//       resource_subtype: "default_task",
-//       //start_on: todaysDate,
-//       parent: null,
-//       workspace: `${workspaceGID}`,
-//       //custom_fields: req.body.custom_fields,
-//     },
-//   };
-//   try {
-//     const response = await fetch(`https://app.asana.com/api/1.0/tasks`, {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//         Authorization: `Bearer ${process.env.PERSONAL_ACCESS_TOKEN}`,
-//       },
-//       body: JSON.stringify(newTask),
-//     });
-//     const data = await response.json();
-
-//     //console.log(data);
-//     //res.status(201).json(data);
-//     return data;
-//   } catch (err) {
-//     console.error(err);
-//     res.status(400).json(err);
-//   }
-// }
-// async function getDeal() {}
-
 async function promptUser() {
   let runAgain = true;
   while (runAgain) {
@@ -195,12 +193,43 @@ async function promptUser() {
       const userGID = await findUser();
       const teamGID = await findTeam(workspaceGID, userGID);
       const projectGID = await findProject(teamGID);
-      console.log(projectGID);
       const nextInteraction = await mainMenu();
+      const dealInfo = {
+        // workspaceGID: 1111138376302363,
+        // teamGID: 1202402175058585,
+        // projectGID: 1202402175058587,
+        // assigneeGID: 1202402171924303,
+        id: "DT22-6826-A",
+        issuer: "SpaceX",
+        broker: "Gordon-Rogoff",
+        operations: "Hutler",
+        buyer: "Kenny",
+        seller: "Confidential91",
+        quantity: 14286,
+        price: 70.0,
+        type: "Fund Direct",
+        total: 1000020.0,
+        commission: 50000.0,
+        date: "06-06-2022",
+        buyerStatus: "NO ACTION",
+        sellerStatus: "NO ACTION",
+        // custom_fields: {
+        //   1202448347979193: "Kenny",
+        //   1202448385422158: "1202448385422161",
+        //   1202453114761807: 70.0,
+        //   1202448400714028: "Confidential91",
+        //   1202448362499526: "SpaceX",
+        //   1202448476119897: "DT22-6826-A",
+        //   1202448402746765: 50000.0,
+        //   1202448403702940: 14286,
+        //   1202448367301577: 1000020.0,
+        // },
+      };
 
       switch (nextInteraction) {
         case "Add a deal?":
-          // addDeal(userGID);
+          console.log("Adding a deal...");
+          addDeal(workspaceGID, projectGID, userGID, dealInfo);
           break;
       }
       let answers = await inquirer.prompt([
