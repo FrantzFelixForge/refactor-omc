6eed3c0a9af0d2d73a0c47cebc16de9ab0f974df
frantz felix <frantz.felix@forgeglobal.com>
2022-06-22 14:40:44 -0700

    Removed asana functions and put them in utils folder to clean up server code.

--- server.js
+++ server.js
@@ -8,8 +8,15 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 const inquirer = require("inquirer");
-const fetch = require(`node-fetch`);
-const asana = require(`asana`);
+
+const {
+  findWorkspace,
+  findUser,
+  findTeam,
+  findProject,
+  addDeal,
+  getDeal,
+} = require("./utils/asanaUtil");
 
 app.use(morgan(`dev`));
 app.use(express.urlencoded({ extended: true }));
@@ -31,149 +38,6 @@ app.listen(PORT, function () {
 
 // }
 
-async function findWorkspace() {
-  try {
-    const response = await fetch(
-      `https://app.asana.com/api/1.0/workspaces?opt_fields=name,gid`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.PERSONAL_ACCESS_TOKEN}`,
-        },
-      }
-    );
-    const { data } = await response.json();
-    const choiceObj = {};
-    const choiceArray = [];
-    data.map(function (workspace) {
-      choiceObj[workspace.name] = workspace.gid;
-      choiceArray.push(workspace.name);
-    });
-    let answers = await inquirer.prompt([
-      {
-        type: "list",
-        name: "workspace",
-        message: "What workspace are you apart of?",
-        choices: choiceArray,
-      },
-    ]);
-    return choiceObj[answers.workspace];
-  } catch (error) {
-    console.log(error);
-  }
-}
-async function findUser() {
-  const client = asana.Client.create().useAccessToken(
-    process.env.PERSONAL_ACCESS_TOKEN
-  );
-  const currentUser = await client.users.me().then(function (me) {
-    return me;
-  });
-  // console.dir(currentUser, { depth: null });
-  return currentUser.gid;
-}
-async function findTeam(workspaceGID, userGID) {
-  const client = asana.Client.create().useAccessToken(
-    process.env.PERSONAL_ACCESS_TOKEN
-  );
-  try {
-    const { data } = await client.teams.getTeamsForUser(`${userGID}`, {
-      workspace: `${workspaceGID}`,
-      opt_fields: "gid",
-      opt_fields: "name",
-    });
-    const choiceObj = {};
-    const choiceArray = [];
-    data.map(function (team) {
-      choiceObj[team.name] = team.gid;
-      choiceArray.push(team.name);
-    });
-    let answers = await inquirer.prompt([
-      {
-        type: "list",
-        name: "team",
-        message: "What team is the project in?",
-        choices: choiceArray,
-      },
-    ]);
-    return choiceObj[answers.team];
-  } catch (error) {
-    console.log(error);
-    return error;
-  }
-
-  // client.teams.dispatchGet();
-}
-async function findProject(teamGID) {
-  const client = asana.Client.create().useAccessToken(
-    process.env.PERSONAL_ACCESS_TOKEN
-  );
-  try {
-    const { data } = await client.projects.getProjectsForTeam(teamGID, {
-      opt_fields: "gid",
-      opt_fields: "name",
-    });
-    const choiceObj = {};
-    const choiceArray = [];
-    data.map(function (project) {
-      choiceObj[project.name] = project.gid;
-      choiceArray.push(project.name);
-    });
-    let answers = await inquirer.prompt([
-      {
-        type: "list",
-        name: "project",
-        message: "What project did you want to select?",
-        choices: choiceArray,
-      },
-    ]);
-    return choiceObj[answers.project];
-  } catch (error) {
-    console.log(error);
-    return error;
-  }
-}
-
-async function addDeal(workspaceGID, projectGID, userGID, dealInfo) {
-  const newDeal = {
-    data: {
-      approval_status: "pending",
-      assignee: `${userGID}`,
-      assignee_status: "upcoming",
-      completed: false,
-      // due_on: `${req.body.taskCompletionDate}`,
-      //liked: true,
-      name: `${dealInfo.issuer} | ${dealInfo.seller}/${dealInfo.buyer} | $${dealInfo.price}`,
-      notes: `Term Sheet notes here`,
-      projects: [`${projectGID}`],
-      resource_subtype: "default_task",
-      //start_on: todaysDate,
-      parent: null,
-      workspace: `${workspaceGID}`,
-      //custom_fields: req.body.custom_fields,
-    },
-  };
-  try {
-    const response = await fetch(`https://app.asana.com/api/1.0/tasks`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.PERSONAL_ACCESS_TOKEN}`,
-      },
-      body: JSON.stringify(newDeal),
-    });
-    const data = await response.json();
-
-    console.log(data);
-    //res.status(201).json(data);
-    return data;
-  } catch (err) {
-    console.error(err);
-    res.status(400).json(err);
-  }
-}
-async function getDeal() {}
 async function mainMenu() {
   let answers = await inquirer.prompt([
     {
@@ -225,6 +89,7 @@ async function promptUser() {
         //   1202448367301577: 1000020.0,
         // },
       };
+      dealInfo["wireAmount"] = dealInfo.total + dealInfo.commission;
 
       switch (nextInteraction) {
         case "Add a deal?":
@@ -248,3 +113,5 @@ async function promptUser() {
 }
 
 promptUser();
+
+// Brokers, Ops Person, Share Count, Share Price, Total Consideration, Commission Total, and Wire Amount (but wire amount is not a field that exists right now)
