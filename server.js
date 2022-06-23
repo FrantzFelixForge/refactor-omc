12cd24ebfe2a83c4221b1f92be8282da6c17f7fa
frantz felix <frantz.felix@forgeglobal.com>
2022-06-22 17:21:11 -0700

    Changed while loop to only ask menu questions. Made node exit the process if the user selects no. Also asked the user if they wanted to get all deals in a section, let them select the section they wanted, then showed them all the tasks in that section.

--- server.js
+++ server.js
@@ -5,10 +5,7 @@ const morgan = require(`morgan`);
 const path = require(`path`);
 const routes = require(`./routes`);
 require("dotenv").config();
-const app = express();
-const PORT = process.env.PORT || 3000;
 const inquirer = require("inquirer");
-
 const {
   findWorkspace,
   findUser,
@@ -16,7 +13,13 @@ const {
   findProject,
   addDeal,
   getDeal,
+  getSectionList,
+  getTasksInSection,
 } = require("./utils/asanaUtil");
+const { exit } = require("process");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(morgan(`dev`));
 app.use(express.urlencoded({ extended: true }));
@@ -27,16 +30,6 @@ app.use(routes);
 app.listen(PORT, function () {
   console.log(`Server running at http://localhost:${PORT}`);
 });
-// function makeChoices(dataArray){
-//     const choiceObj = {};
-//     const choiceArray = [];
-//     data.map(function (dataArray) {
-//       choiceObj[dataArray.name] = dataArray.gid;
-//       choiceArray.push(dataArray.name);
-//     });
-//     return choiceArray;
-
-// }
 
 async function mainMenu() {
   let answers = await inquirer.prompt([
@@ -44,58 +37,67 @@ async function mainMenu() {
       type: "list",
       name: "menu",
       message: "What do you want to do next?",
-      choices: ["Get all deals?", "Add a deal?"],
+      choices: ["Get all deals.", "Add a deal.", "Get all deals in a section."],
     },
   ]);
   return answers.menu;
 }
 async function promptUser() {
   let runAgain = true;
-  while (runAgain) {
-    try {
-      const workspaceGID = await findWorkspace();
-      const userGID = await findUser();
-      const teamGID = await findTeam(workspaceGID, userGID);
-      const projectGID = await findProject(teamGID);
+  // while (runAgain) {
+  try {
+    const dealInfo = {
+      // workspaceGID: 1111138376302363,
+      // teamGID: 1202402175058585,
+      // projectGID: 1202402175058587,
+      // assigneeGID: 1202402171924303,
+      id: "DT22-6826-A",
+      issuer: "SpaceX",
+      broker: "Gordon-Rogoff",
+      operations: "Hutler",
+      buyer: "Kenny",
+      seller: "Confidential91",
+      quantity: 14286,
+      price: 70.0,
+      type: "Fund Direct",
+      total: 1000020.0,
+      commission: 50000.0,
+      date: "06-06-2022",
+      buyerStatus: "NO ACTION",
+      sellerStatus: "NO ACTION",
+      // custom_fields: {
+      //   1202448347979193: "Kenny",
+      //   1202448385422158: "1202448385422161",
+      //   1202453114761807: 70.0,
+      //   1202448400714028: "Confidential91",
+      //   1202448362499526: "SpaceX",
+      //   1202448476119897: "DT22-6826-A",
+      //   1202448402746765: 50000.0,
+      //   1202448403702940: 14286,
+      //   1202448367301577: 1000020.0,
+      // },
+    };
+    dealInfo["wireAmount"] = dealInfo.total + dealInfo.commission;
+
+    const workspaceGID = await findWorkspace();
+    const userGID = await findUser();
+    const teamGID = await findTeam(workspaceGID, userGID);
+    const projectGID = await findProject(teamGID);
+    while (runAgain) {
       const nextInteraction = await mainMenu();
-      const dealInfo = {
-        // workspaceGID: 1111138376302363,
-        // teamGID: 1202402175058585,
-        // projectGID: 1202402175058587,
-        // assigneeGID: 1202402171924303,
-        id: "DT22-6826-A",
-        issuer: "SpaceX",
-        broker: "Gordon-Rogoff",
-        operations: "Hutler",
-        buyer: "Kenny",
-        seller: "Confidential91",
-        quantity: 14286,
-        price: 70.0,
-        type: "Fund Direct",
-        total: 1000020.0,
-        commission: 50000.0,
-        date: "06-06-2022",
-        buyerStatus: "NO ACTION",
-        sellerStatus: "NO ACTION",
-        // custom_fields: {
-        //   1202448347979193: "Kenny",
-        //   1202448385422158: "1202448385422161",
-        //   1202453114761807: 70.0,
-        //   1202448400714028: "Confidential91",
-        //   1202448362499526: "SpaceX",
-        //   1202448476119897: "DT22-6826-A",
-        //   1202448402746765: 50000.0,
-        //   1202448403702940: 14286,
-        //   1202448367301577: 1000020.0,
-        // },
-      };
-      dealInfo["wireAmount"] = dealInfo.total + dealInfo.commission;
 
       switch (nextInteraction) {
-        case "Add a deal?":
+        case "Add a deal.":
           console.log("Adding a deal...");
           addDeal(workspaceGID, projectGID, userGID, dealInfo);
           break;
+        case "Get all deals in a section.":
+          const { sectionGID, sectionName } = await getSectionList(projectGID);
+          console.log(`Getting all tasks in ${sectionName}...`);
+          console.table(await getTasksInSection(sectionGID));
+          break;
+        default:
+          console.log("Unknown interaction: " + nextInteraction);
       }
       let answers = await inquirer.prompt([
         {
@@ -106,10 +108,14 @@ async function promptUser() {
         },
       ]);
       answers.continue === "yes" ? (runAgain = true) : (runAgain = false);
-    } catch (error) {
-      console.log(error);
+      if (!runAgain) {
+        exit();
+      }
     }
+  } catch (error) {
+    console.log(error);
   }
+  // }
 }
 
 promptUser();
