3c4e519597da3d7730621a24ead9bdcbfde130d0
frantz felix <frantz.felix@forgeglobal.com>
2022-07-06 17:26:23 -0700

    Added section and webhook controllers to create webhooks, get them and delete them. Section controller grabs a single section by gid or all of them in a project by project gid.

--- controllers/index.js
+++ controllers/index.js
@@ -2,4 +2,6 @@ const Task = require("./task");
 const Tag = require("./tag");
 const User = require("./user");
 const Story = require("./story");
-module.exports = { Task, Tag, User, Story };
+const Webhook = require("./webhook");
+const Section = require("./section");
+module.exports = { Task, Tag, User, Story, Webhook, Section };
