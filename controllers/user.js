b6f3d3401c6ebd61ca4b05e51da51d7376ba6bb5
frantz felix <frantz.felix@forgeglobal.com>
2022-07-11 10:35:11 -0700

    Added method to get users by team.

--- controllers/user.js
+++ controllers/user.js
@@ -3,9 +3,9 @@ require(`dotenv`).config();
 
 class User {
   constructor() {
-    this.client = asana.Client.create().useAccessToken(
-      process.env.PERSONAL_ACCESS_TOKEN
-    );
+    this.client = asana.Client.create({
+      logAsanaChangeWarnings: false,
+    }).useAccessToken(process.env.PERSONAL_ACCESS_TOKEN);
   }
 
   /* /users/{user_gid} */
@@ -13,6 +13,16 @@ class User {
     const result = await this.client.users.getUser(userGID, {
       opt_fields: "name",
     });
+    return result;
+  }
+
+  async getUsersByTeam(teamGID) {
+    const gid = teamGID || process.env.ENG_TEAM_GID;
+
+    const result = await this.client.users.getUsersForTeam(gid, {
+      opt_fields: "name",
+    });
+
     return result;
   }
 }
